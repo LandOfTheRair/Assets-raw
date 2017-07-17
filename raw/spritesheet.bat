2628dc2666a864c470f34d6a58ca8723f9149169
Kyle Kemp <kyle@seiyria.com>
2017-07-17 11:47:27 -0500

    add swimming sprites

--- raw/spritesheet.bat
+++ raw/spritesheet.bat
@@ -4,5 +4,6 @@ SET SHEETWIDTH=2048
 SET PATH=%~1%
 
 IF NOT "%PATH%"=="%PATH:creatures=%" SET SHEETWIDTH=2560
+IF NOT "%PATH%"=="%PATH:swimming=%" SET SHEETWIDTH=512
 
 D:/aseprite/aseprite -b --sheet "%~1.png" --sheet-width %SHEETWIDTH% "%~1/*.ase"
